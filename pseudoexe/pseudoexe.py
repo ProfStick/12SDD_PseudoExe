bcde3b87f5347bf4e8e3d555db7da67b2f2b9597
Geoff Goldrick <14170100+ProfStick@users.noreply.github.com>
2020-06-16 14:01:29 +1000

    changed directory strudture for RTD

---
+++ pseudoexe/pseudoexe.py
@@ -0,0 +1,59 @@
+"""A lexer for PseudoExe language project.
+
+Defines a lexicon for the PseudoExe language and performs lexical analysis.
+
+  Typical usage example:
+
+  foo = SampleClass()
+  
+  bar = foo.public_method(required_variable, optional_variable=42)
+"""
+
+class Lexer(object):
+    """Summary of class here.
+
+    Longer class information....
+    
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
+
+    def __init__(self, likes_spam=False):
+        """Inits SampleClass with blah."""
+        self.likes_spam = likes_spam
+        self.eggs = 0
+
+    def public_method(self):
+        """Longer description of desired functionality
+
+        Args:
+            required_variable: A required argument
+            optional_variable: An optional argument
+
+        Returns:
+            None: but if it did you would describe it here
+
+        Raises:
+            NoError: but if it did you would describe it here
+        """
+        return None
+
+def function_name(required_variable, optional_variable=None):
+    """Short description.
+
+    Longer description of desired functionality
+
+    Args:
+        required_variable: A required argument
+        optional_variable: An optional argument
+
+    Returns:
+        None: but if it did you would describe it here
+
+    Raises:
+        NoError: but if it did you would describe it here
+    """
+    return None
+
+    
